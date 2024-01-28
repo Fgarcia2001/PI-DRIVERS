14ccd998fcbf57d68d0a48af5b30ef32bf7cbaa9
francog <franco2001garcia@gmail.com>
2024-01-28 20:18:45 -0300

    server config

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
+require("dotenv").config();
 const server = require("./src/server");
 const { conn } = require("./src/db.js");
-const PORT = 3001;
-
+const { DB_PORT } = process.env();
 conn
   .sync({ alter: true })
   .then(() => {
     server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
+      console.log(`Server listening on port ${DB_PORT}`);
     });
   })
   .catch((error) => console.error(error));
